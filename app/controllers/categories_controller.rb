c00efad5b66efa3a862a24daf2c7dfe01f38fc6d
Caitlin Hoffman <choff999@gmail.com>
2016-05-25 19:16:02 -0400

    Add files to categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,10 @@
 class CategoriesController < ApplicationController
-def index
-  @categories = Category.all
-end
+	def index
+  	@categories = Category.all
+	end
 
-def show
-  @category = Category.find(params[:id])
-  @category_films = @category.films
-end
+	def show
+  	@category = Category.find_by(id: params[:id])
+  	@category_films = @category.films
+	end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
-	belongs_to :film
+	has_many :films
 
 	validates :name, presence: true
 
