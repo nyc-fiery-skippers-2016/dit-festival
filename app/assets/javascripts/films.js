ce46601a3d4d45aea4c0a0de37ddb2825371b674
RobertReith <robertmreith@gmail.com>
2016-05-26 15:42:31 -0400

    Complete ajax for reviews

--- app/assets/javascripts/films.js
+++ app/assets/javascripts/films.js
@@ -15,6 +15,23 @@ $(document).ready(function(){
       $('#review-form-holder').append(response);
       $('.write-review').hide();
     });
+  });
 
+  $('#review-form-holder').on('submit', '#new_review', function(event){
+    event.preventDefault();
+
+    var $target = $(event.target)
+
+    requestOptions = {
+      url: $target.attr('action'),
+      method: $target.attr('method'),
+      data: $target.serialize()
+    }
+
+    $.ajax(requestOptions).done(function(response){
+      $('#review-list').append(response);
+      $('#review-form-holder').empty();
+      $('.write-review').show();
+    });
   });
 });
