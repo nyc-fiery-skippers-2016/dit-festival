ab2015fc4d01568f7dc9e9e08e3492176a623ab1
hannataylor <hanna.taylor91@gmail.com>
2016-05-26 16:54:58 -0400

    More CSS and view content

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
+    @user = User.new(user_params.merge(judge: false))
 
     if @user.save
       session[:user_id] = @user.id
