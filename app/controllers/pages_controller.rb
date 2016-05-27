d8e8a142c843de0365a8c8e8e1d22d9aef1db85b
Ayaz Uddin <ayaz2589@gmail.com>
2016-05-26 22:28:37 -0400

    update winners page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,11 @@
 class PagesController < ApplicationController
 
 	def show
-		@category1 = Category.find(1).films.sort_by { |film| film.score }
-		@category2 = Category.find(2).films.sort_by { |film| film.score }
-		@category3 = Category.find(3).films.sort_by { |film| film.score }
-		@category4 = Category.find(4).films.sort_by { |film| film.score }
-		@category5 = Category.find(5).films.sort_by { |film| film.score }
-		@category6 = Category.find(6).films.sort_by { |film| film.score }
+		@category1 = Category.find(1).films.sort_by { |film| film.average_rating }
+		@category2 = Category.find(2).films.sort_by { |film| film.average_rating }
+		@category3 = Category.find(3).films.sort_by { |film| film.average_rating }
+		@category4 = Category.find(4).films.sort_by { |film| film.average_rating }
+		@category5 = Category.find(5).films.sort_by { |film| film.average_rating }
+		@category6 = Category.find(6).films.sort_by { |film| film.average_rating }
 	end
 end
